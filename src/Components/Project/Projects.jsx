669e5e36b741a6005678ece15f16fa4b382c82ce
LuciferGod6 <prasadgourav46@gmail.com>
2023-08-07 02:23:40 +0530

    added

--- src/Components/Project/Projects.jsx
+++ src/Components/Project/Projects.jsx
@@ -20,15 +20,15 @@ export const Projects = () => {
       <div style={{ borderBottom: "2px solid #00abf0", width: "80px", marginBottom: "10px", marginTop: "5px", borderBottomRightRadius: "5px", borderTopRightRadius: "5px" }}  ></div>
       <p style={{ textAlign: "left", marginTop: "0px", paddingBottom: "10px" }} >What I Do</p>
       <div className='projectMainContainer' >
-        <div className='topBox'  >
+        <div className='topBox'  class="project-card" >
           <div >
             <img src={baillie} alt="" className='projectImages' />
           </div>
           <div>
             <h1 class="project-title"  >Bialetti</h1>
-            <p>Bialetti Italia is a captivating web application that pays homage to the renowned Italian brand, Bialetti, famous for its exceptional coffee machines, cookware, and small kitchen appliances.</p>
-            <h4>Tech Stack :</h4>
-            <div className='projectSkills' >
+            <p class="project-description" >Bialetti Italia is a captivating web application that pays homage to the renowned Italian brand, Bialetti, famous for its exceptional coffee machines, cookware, and small kitchen appliances.</p>
+            <h4 class="project-tech-stack" >Tech Stack :</h4>
+            <div  className='projectSkills' >
               <img src={react} alt="react" />
               <img src={redux} alt="redux" />
               <img src={charka} alt="chakra" />
@@ -42,8 +42,8 @@ export const Projects = () => {
           </div>
           <div>
             <h1 class="project-title" >Overstock</h1>
-            <p>Overstock.com, Inc. is an American internet retailer selling primarily furniture headquartered in Midvale, Utah, near Salt Lake City. The company sells home decor, furniture, bedding, and many other goods that are closeout merchandise.</p>
-            <h4>Tech Stack :</h4>
+            <p class="project-description" >Overstock.com, Inc. is an American internet retailer selling primarily furniture headquartered in Midvale, Utah, near Salt Lake City. The company sells home decor, furniture, bedding, and many other goods that are closeout merchandise.</p>
+            <h4 class="project-tech-stack" >Tech Stack :</h4>
             <div className='projectSkills' >
               <img src={html} alt="react" />
               <img src={css} alt="redux" />
@@ -58,13 +58,13 @@ export const Projects = () => {
           </div>
           <div>
             <h1 class="project-title" >Alcazar</h1>
-           <p>Alcazar is one of the most used travel agency website.
+           <p class="project-description" >Alcazar is one of the most used travel agency website.
            Discover a world of convenience and luxury
              as we offer a comprehensive array of facilities to elevate every aspect of your getaway.
               From handpicked accommodations to curated itineraries, we specialize in crafting unforgettable 
               vacations.
              </p>
-            <h4>Tech Stack :</h4>
+            <h4 class="project-tech-stack" >Tech Stack :</h4>
             <div className='projectSkills' >
               <img src={react} alt="react" />
               <img src={redux} alt="redux" />
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -19,35 +19,35 @@ export const Skills = () => {
       <div style={{borderBottom:"2px solid #00abf0",width:"60px" ,marginBottom:"10px", marginTop:"5px",borderBottomRightRadius:"5px",borderTopRightRadius:"5px" }}  ></div>
       <p style={{marginTop:"0"}}>What I know</p>
       <div className='icon_Div' >
-        <div><img src={html5} alt="csa" />
-          <p>HTML</p>
+        <div  ><img class="skills-card-img" src={html5} alt="csa" />
+          <p class="skills-card-name" >HTML</p>
         </div>
-        <div><img src={css} alt="css" />
-          <p>CSS</p>
+        <div><img class="skills-card-img" src={css} alt="css" />
+          <p class="skills-card-name" >CSS</p>
         </div>
-        <div><img src={js} alt="js" />
-          <p>JS</p>
+        <div><img class="skills-card-img" src={js} alt="js" />
+          <p  class="skills-card-name" >JS</p>
         </div>
-        <div><img src={java} alt="js" />
-          <p>JAVA</p>
+        <div><img class="skills-card-img" src={java} alt="js" />
+          <p class="skills-card-name">JAVA</p>
         </div>
-        <div><img src={mern} alt="mern" />
-          <p>MERN</p>
+        <div><img class="skills-card-img" src={mern} alt="mern" />
+          <p class="skills-card-name" >MERN</p>
         </div>
-        <div><img src={redux} alt="redux" />
-          <p>REDUX</p>
+        <div><img class="skills-card-img" src={redux} alt="redux" />
+          <p class="skills-card-name" >REDUX</p>
         </div>
-        <div><img src={react} alt="react" />
-          <p>REACT</p>
+        <div><img class="skills-card-img" src={react} alt="react" />
+          <p class="skills-card-name" >REACT</p>
         </div>
-        <div><img src={mongodb} alt="mongodb" />
-          <p>MONGODB</p>
+        <div><img class="skills-card-img" src={mongodb} alt="mongodb" />
+          <p class="skills-card-name" >MONGODB</p>
         </div>
-        <div><img src={netlify} alt="netlify" />
-          <p>NETLIFY</p>
+        <div><img class="skills-card-img" src={netlify} alt="netlify" />
+          <p class="skills-card-name" >NETLIFY</p>
         </div>
-        <div><img src={charka} alt="netlify" />
-          <p>CHAKRA UI</p>
+        <div><img class="skills-card-img" src={charka} alt="netlify" />
+          <p class="skills-card-name" >CHAKRA UI</p>
         </div>
       </div>
     </div>
